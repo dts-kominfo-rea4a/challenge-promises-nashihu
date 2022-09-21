ea9c619a5f445234f03242e84c4fc364ab5cc038
nashihu <ahmad.n.id@gmail.com>
2022-09-21 19:01:49 +0700

    submit

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (ekspresiPenonton) => {
+  return new Promise((resolve, _) => {
+    if (ekspresiPenonton == 'marah') {
+      resolve(4)
+    } else {
+      resolve(2)
+    }
+  })
+};
 
 module.exports = {
   promiseOutput,
